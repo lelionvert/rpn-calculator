92b33e39261fad165382133adcd64f02642ec501
vdsalexandre <alexandre.vandensteen@gmail.com>
2020-03-25 09:29:18 +0100

    env test ok

---
+++ src/test/java/lcdlv/CalculatorTest.java
@@ -0,0 +1,13 @@
+package lcdlv;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+
+    @Test
+    public void envTest() {
+        assertThat(true).isTrue();
+    }
+}
